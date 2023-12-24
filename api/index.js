a4ca60c5620bd1ef90ae6f270ce5fd13e06783e1
Krintox <87925419+Krintox@users.noreply.github.com>
2023-12-24 21:17:00 +0530

    ...

--- api/index.js
+++ api/index.js
@@ -12,14 +12,14 @@ const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
-const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+const secret = 'your salt';
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-const mongoUrl = "mongodb+srv://shashank:shashank@cluster0.prqk0n3.mongodb.net/?retryWrites=true&w=majority";
+const mongoUrl = "mongodb+srv://username:password@cluster0.prqk0n3.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(mongoUrl, {
     useNewUrlParser:true
